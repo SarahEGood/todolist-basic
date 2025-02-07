afeabb6a3609481f1d46a9ca94a69cae93c0ed02
Sarah Good <sarah.elizabeth.good@gmail.com>
2025-02-07 15:48:16 -0800

    very basic create task functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import "./styles.css";
 
-// ToDo List Objects
+var createModal = document.getElementById('modal-create');
+var createbtn = document.getElementById("create");
+var span = document.getElementsByClassName("close")[0];
+var submitcreate = document.getElementById('submitCreate');
+
+document.getElementById('c-dateDue').value = new Date().toISOString().split('T')[0];;
+
+createbtn.onclick = function() {
+    createModal.style.display = 'block';
+}
+
+submitcreate.onclick = function() {
+    createTask();
+    createModal.style.display = 'none';
+}
+
+span.onclick = function() {
+    createModal.style.display = 'none';
+}
+
+// Task Objects
 class Task {
     constructor(title, detail, dateDue, priority, project, parent, children) {
         this.title = title;
@@ -14,4 +34,82 @@ class Task {
         this.modDate = new Date();
     }
 
+    get getJson() {
+        let jsonob = {
+            title: this.title,
+            detail: this.detail,
+            dueDate: this.dateDue,
+            priority: this.priority,
+            project: this.project,
+            parent: this.parent,
+            children: this.children,
+            createdDate: this.createdDate,
+            modDate: this.modDate   
+        };
+        return jsonob;
+    }
+
+}
+
+let myTasks = [];
+
+// Task CRUD functionality
+function createTask() {
+    // Get task info from a form in a create task modal
+    // Optional: May need to create new func or pass param if also allow inline on page
+
+    const title = document.getElementById('c-title').value;
+    const detail = document.getElementById('c-detail').value;
+    const duedate = document.getElementById('c-dateDue').value;
+    const priority = document.getElementById('c-priority').value;
+    let project = document.getElementById('c-project').value;
+    if (project == null) {
+        project = 'Inbox';
+    }
+
+    let newTask = new Task(title, detail, duedate, priority, project,
+        0, 0
+    );
+
+    // Then, append to data and save
+    myTasks.push(newTask);
+    localStorage.setItem('tasks', myTasks);
+
+    // Then, reload tasks
+    reloadTasks();
+
+    // Also, close modal
+
+}
+
+function editTask() {
+    // Same as above, but from an edit modal
+
+    // overwrite task by task id
+
+    //reload page
+}
+
+function deleteTask() {
+    // Trigger on delete button on task list or edit modal
+    // optional: ask user if ok to delete
+    // Delete from data and reload list
+}
+
+// Display tasks
+function reloadTasks() {
+    // Clear task list div
+    const taskList = document.getElementById('tasks');
+    taskList.innerHTML = '';
+
+    // If no json for tasks, create one
+    // else, load the existing json
+    for (let i=0; i<myTasks.length; i++) {
+        let taskInfo = myTasks[i];
+        const newRow = document.createElement('div');
+        newRow.innerHTML = taskInfo.title + ' ' + taskInfo.detail;
+        taskList.appendChild(newRow);
+    }
+
+    // iterate through json of tasks to display them on modal
 }
